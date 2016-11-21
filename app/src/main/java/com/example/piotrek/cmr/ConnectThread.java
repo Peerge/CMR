41e412b481622df08d66d751317231d25ada90a8
Piotr <piotrekrg8@gmail.com>
2016-11-21 14:23:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/piotrek/cmr/ConnectThread.java
@@ -0,0 +1,104 @@
+package com.example.piotrek.cmr;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by piotrek on 21.11.16.
+ */
+
+public class ConnectThread implements Runnable {
+
+    private Socket socket ;
+    private String ip = "217.153.10.141";
+    private int port = 6503;
+    public boolean isConnect = false;
+    DataInputStream is;
+    DataOutputStream os;
+
+    public ConnectThread(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public void run() {
+        try {
+            socket = new Socket(ip, port);
+            is = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            os = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            isConnect = socket.isConnected();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static byte[] receive(DataInputStream is) throws Exception
+    {
+        try
+        {
+            byte[] inputData = new byte[1024];
+            is.read(inputData);
+            return inputData;
+        }
+        catch (Exception exception)
+        {
+            throw exception;
+        }
+    }
+
+    public static void send(DataOutputStream os, byte[] byteData) throws Exception
+    {
+        try
+        {
+            os.write(byteData);
+            os.flush();
+        }
+        catch (Exception exception)
+        {
+            throw exception;
+        }
+    }
+
+    public static byte[] getMsg()
+    {
+        byte first = 0x68;
+        byte frameLength = 0x5;
+        byte[] receiveAdr = new byte[2];
+        receiveAdr[0] = (byte) 0xff;
+        receiveAdr[1] = (byte) 0xff;
+        byte[] sendAdr = new byte[2];
+        sendAdr[0] = 0x1;
+        sendAdr[1] = 0x15;
+        byte msg = 0x09;
+        byte answer = 0x00;
+        byte[] CRC = new byte[2];
+        CRC[0] = (byte) 0x9A;
+        CRC[1] = 0x62;
+        byte end = 0x16;
+
+        byte[] request = new byte[11];
+        request[0] = first;
+        request[1] = frameLength;
+        request[2] = receiveAdr[0];
+        request[3] = receiveAdr[1];
+        request[4] = sendAdr[0];
+        request[5] = sendAdr[1];
+        request[6] = msg;
+        request[7] = answer;
+        request[8] = CRC[0];
+        request[9] = CRC[1];
+        request[10] = end;
+
+        return request;
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/piotrek/cmr/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.piotrek.cmr;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private String ip = "217.153.10.141";
+    private int port = 6503;
+    private Button btn_connect = null;
+    private Button btn_send = null;
+    private TextView text_receive = null;
+
+
+    ConnectThread connectThread;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_connect = (Button) this.findViewById(R.id.btn_connect);
+        btn_send = (Button) this.findViewById(R.id.btn_send);
+        text_receive = (TextView) this.findViewById(R.id.text_receive);
+
+
+        btn_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connectThread = new ConnectThread(ip, port);
+                new Thread(connectThread).start();
+                Log.d(TAG, "Connection starting");
+                if (connectThread.isConnect) {
+                    btn_connect.setText("Disconnect");
+                }
+            }
+        });
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataInputStream is = null;
+
+                try {
+                    connectThread.send(connectThread.os, connectThread.getMsg());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    byte[] buffer = connectThread.receive(is);
+                    String response = new String(buffer);
+                    text_receive.setText(response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
