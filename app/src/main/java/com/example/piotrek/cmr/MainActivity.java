1872aaf31cb7ade1002e94a01384744940681933
Piotr <piotrekrg8@gmail.com>
2016-11-22 15:08:31 +0100

    Message is sent. Need to build proper frame message.

--- app/src/main/java/com/example/piotrek/cmr/MainActivity.java
+++ app/src/main/java/com/example/piotrek/cmr/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.piotrek.cmr;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         btn_connect = (Button) this.findViewById(R.id.btn_connect);
         btn_restart = (Button) this.findViewById(R.id.btn_restart);
@@ -56,5 +60,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.restart:
+                recreate();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
